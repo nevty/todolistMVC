1f6e19f21737586d5a6db80efcca57336a1735dc
nevty <theacutemace@gmail.com>
2019-05-24 02:02:10 +0700

    Добавил проект

---
+++ src/model.js
@@ -0,0 +1,39 @@
+import { EventEmitter } from './helpers';
+
+class Model extends EventEmitter {
+  constructor(state = []) {
+    super();
+    this.state = state;
+  }
+
+  getItem(id) {
+    return this.state.find(item => item.id == id);
+  }
+
+  addItem(item) {
+    this.state.push(item);
+    this.emit('change', this.state);
+    return item;
+  }
+
+  updateItem(id, data) {
+    const item = this.getItem(id);
+
+    Object.keys(data).forEach(prop => item[prop] = data[prop]);
+
+    this.emit('change', this.state);
+
+    return item;
+  }
+
+  removeItem(id) {
+    const index = this.state.findIndex(item => item.id == id);
+
+    if (index > -1) {
+      this.state.splice(index, 1);
+      this.emit('change', this.state);
+    }
+  }
+}
+
+export default Model;
